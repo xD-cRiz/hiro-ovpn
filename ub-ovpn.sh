c84597abaf4aa911c4a261b4b28a301205f1cc94
xD-cRiz <126428049+xD-cRiz@users.noreply.github.com>
2023-02-27 12:12:41 +0800

    Update ub-ovpn.sh

--- ub-ovpn.sh
+++ ub-ovpn.sh
@@ -645,6 +645,31 @@ PASS="romero"
 useradd $USER
 echo "$USER:$PASS" | chpasswd
 }
+install_menu()
+{
+# download script
+cd /usr/bin
+wget -O menu "https://raw.githubusercontent.com/xD-cRiz/hiro-ovpn/main/menu/menu.sh"
+wget -O usernew "https://raw.githubusercontent.com/xD-cRiz/hiro-ovpn/main/menu/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/xD-cRiz/hiro-ovpn/main/menu/trial.sh"
+wget -O delete "https://raw.githubusercontent.com/xD-cRiz/hiro-ovpn/main/menu/hapus.sh"
+wget -O check "https://raw.githubusercontent.com/xD-cRiz/hiro-ovpn/main/menu/user-login.sh"
+wget -O member "https://raw.githubusercontent.com/xD-cRiz/hiro-ovpn/main/menu/user-list.sh"
+wget -O restart "https://raw.githubusercontent.com/xD-cRiz/hiro-ovpn/main/menu/resvis.sh"
+wget -O info "https://raw.githubusercontent.com/xD-cRiz/hiro-ovpn/main/menu/info.sh"
+wget -O about "https://raw.githubusercontent.com/xD-cRiz/hiro-ovpn/main/menu/about.sh"
+
+chmod +x menu
+chmod +x usernew
+chmod +x trial
+chmod +x delete
+chmod +x check
+chmod +x member
+chmod +x restart
+chmod +x info
+chmod +x about
+}
+
 install_done()
 {
   clear
@@ -675,4 +700,5 @@ install_stunnel
 install_rclocal
 install_iptables
 install_acount
+install_menu
 install_done
